8f97b8f52174117052fedd6fe92470b8df27a1fd
muhsinulfikri <mohpikry90@gmail.com>
2024-10-04 13:29:15 +0700

    add file vercel and fix host

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5001,
-    host: 'localhost',
+    host: 'https://notes-app-backend-vert.vercel.app/',
     routes: {
       cors: {
         origin: ['*'],
